8b48fed6c90e2dd253f0ccceefca6c260e3dc7c4
jfelic <felicianojulian1@gmail.com>
2024-07-17 13:51:03 -0400

    Initial Commit

---
+++ testingSwiftUI/testingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftUIApp.swift
+//  testingSwiftUI
+//
+//  Created by Julian Feliciano on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct testingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
