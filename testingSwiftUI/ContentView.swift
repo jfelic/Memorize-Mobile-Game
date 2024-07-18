c5354d45c1394168123e0ed01f8343eec333f64e
jfelic <felicianojulian1@gmail.com>
2024-07-18 12:34:17 -0400

    Initial commit

--- testingSwiftUI/ContentView.swift
+++ testingSwiftUI/ContentView.swift
@@ -7,17 +7,106 @@
 
 import SwiftUI
 
+//MARK: ContentView
 struct ContentView: View {
+    
+    //Theme arrays
+    let halloweenEmojis = ["🎃", "👻", "🕸️", "🕷️", "🧙‍♀️", "🧟‍♂️", "🧛‍♂️", "💀", "⚰️", "🪦", "🌕", "🧛‍♀️", "🧙‍♂️", "🧟‍♀️", "🍬"]
+    let summerEmojis = ["🌞", "🏖️", "🌴", "🍉", "🍦", "🌊", "🏄‍♂️", "👙", "🌺", "🍹", "🏊‍♀️", "🕶️", "🐚", "🏝️", "🩴"]
+    let xmasEmojis = ["🎄", "🎅", "🤶", "🎁", "🦌", "⛄", "❄️", "🔔", "🕯️", "🍪", "🥛", "🌟", "🧦", "🎀", "🛷"]
+
+    //Default card count
+    @State var cardCount: Int = 4
+    //Default chosen theme
+    @State var chosenTheme: [String] = ["🎃", "👻", "🕸️", "🕷️"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Memorize!").font(.largeTitle)
+            ScrollView{
+                cards
+                buttons
+            }
+            Spacer()
+        }.padding()
     }
-}
+    
+    //MARK: ContentView Components
+    var cards: some View {
+        
+        return LazyVGrid(columns: [GridItem(), GridItem()]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: chosenTheme[index]).aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var buttons: some View {
+        HStack {
+            halloweenButton
+            Spacer()
+            summerButton
+            Spacer()
+            xmasButton
+            
+        }.padding().imageScale(.large).font(.largeTitle)
+    }
+    
+    var halloweenButton: some View {
+        Button(action: {
+            chosenTheme = halloweenEmojis
+        }, label: {
+            Text("👻")
+        })
+    }
+    
+    var summerButton: some View {
+        Button(action: {
+            chosenTheme = summerEmojis
+        }, label: {
+            Text("☀️")
+        })
+    }
+    
+    var xmasButton: some View {
+        Button(action: {
+            chosenTheme = xmasEmojis
+            print("Chosen theme: \(chosenTheme)")
+        }, label: {
+            Text("🎅🏻")
+        })
+    }
+    
+    //MARK: Functions
+
+}//end of ContentView
+
+//MARK: CardView
+struct CardView: View {
+    
+    let content: String
+    @State var isFaceUp = false// views are immutable
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }//end of body
+}//end of CardView
+
 
 #Preview {
     ContentView()
